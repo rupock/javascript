ad4fae1d0cc0c8cda60e270483ade34c6c338e66
Ahmed Chowdhury <rupock@yahoo.com>
2018-01-26 12:04:51 -0500

    check-balance.js created

---
+++ check-balance.js
@@ -0,0 +1,32 @@
+/*
+ * Programming Quiz - Checking Your Balance (3-5)
+ */
+
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance =325.00;
+var checkBalance = true;
+var isActive = true;
+console.log("using the ATM to Check balance.");
+if (checkBalance===true)
+{
+    if((isActive===true)&&(balance>0.0))git
+    {
+console.log("Your balance is $"+balance.toFixed(2)+".");
+    }
+    if(isActive!==true)
+    {
+console.log("Your account is no longer active");
+    }
+    if(balance===0)
+    {
+console.log("Your account is empty.");
+    }
+    if(balance<0.0){
+        console.log("Your balance is negative. please contact bank.");
+
+    }
+}
+else if(checkBalance===false){
+
+    console.log("Thank you.Have a nice day!");
+}
