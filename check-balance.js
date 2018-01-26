e596f8a8e9a35d1eb15aeb9149de1e356bd25a58
rupock <rupock@yahoo.com>
2018-01-26 12:33:08 -0500

    isActive is true chang  fro github page

--- check-balance.js
+++ check-balance.js
@@ -5,7 +5,7 @@
 // change the values of `balance`, `checkBalance`, and `isActive` to test your code
 var balance =325.00;
 var checkBalance = true;
-var isActive = false;
+var isActive = true;
 console.log("using the ATM to Check balance.");
 if (checkBalance===true)
 {
